5400f5b7604ccffd6e5ead31234330ef7d82c041
TSHEPO-FRONTDEV <tshepotefo21@gmail.com>
2023-12-11 02:34:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  }
+  useEffect(() => {
+    fetchQuote();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='App'>
+        <div id="quote-box">
+          <p id="text">{quote}</p>
+          <p id="author">{author}</p>
+          <button id="new-quote" onClick={fetchQuote}>New quote</button>
+          <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}>Tweet quote</a>
+
+        </div>
+      </div>
+  )
 }
 
 export default App;
